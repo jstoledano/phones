8002ab9fbfeffbc7c44ee07f94e63455329764c9
Javier Sanchez Toledano <js.toledano@me.com>
2018-07-23 22:05:07 -0500

    Nuevas funcionalidades

---
+++ src/apps/tracker/models.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+from django.db import models
+
+
+class Tracker(models.Model):
+    tracker = models.CharField('Folio de Notificación', max_length=13)
+    clave_elector = models.CharField(max_length=18)
+    folio = models.CharField(max_length=13, primary_key=True)
+    paterno = models.CharField(max_length=50)
+    materno = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=50)
+    phone = models.CharField('Teléfono', max_length=10)
+
+    #
--- src/core/urls.py
+++ src/core/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
-from apps.llamadas.views import ReporteView
+from apps.tracker.views import ReporteView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
